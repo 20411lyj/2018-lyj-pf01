cda9a38ce42a031fa3048f83975a7d9941208ee3
20411lyj <lpdt0079@naver.com>
2019-01-03 15:16:51 +0900

    0103반응형 웹페이지

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,24 @@
-/***** 공통사항 변수 선언******/
+/***** 공통사항 변수 선언 ******/
 var bar = $(".navs_mo");
 var bar2 = $(".nav_close");
 var nav = $(".navs_mo_sub");
 var navWid = nav.width();
 
-
-/***** 반응형/높이를 위한 resize******/
+/***** 반응형/높이를 위한 resize ******/
 $(window).resize(function(){
-	navInit(); //모바일 네비게이션 가리기
-	banInit(); //배너 auto height
+	navInit();	//모바일 네비게이션 가리기
+	banInit();	//배너 Auto height
 }).trigger("resize");
 
-/***** 메인 배너******/
-function banInit(){
+/***** 메인 배너 ******/
+function banInit() {
 	$(".banner_wrap").height($(".banner_wrap > li").height());
-
 }
 
-
-/***** 모바일 네비게이션******/
+/***** 모바일 네비게이션 ******/
 bar.click(navToggle);
 bar2.click(navToggle);
-function navInit(){
+function navInit() {
 	navWid = nav.width();
 	if($(window).width() > 768) navHide();
 	else navToggle();
